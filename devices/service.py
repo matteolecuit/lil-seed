7393c3439f3aca19aed0986d066156d24aec8485
Hugo <hugo.huet.14@gmail.com>
2021-03-18 12:02:18 +0100

    add data storage

--- devices/service.py
+++ devices/service.py
@@ -1,9 +1,23 @@
 import requests
+import pickle
 
 def callApi(data):
     url = "localhost:5000"
     print("Call to API :", url, "with data :", data, "\n")
+    
     try:
         requests.post(url, data)
     except:
         pass
+
+def loadData():
+    file = open('data.txt', 'rb')
+    data = pickle.load(file)
+    file.close()
+    return data
+        
+
+def storeData(data):
+    file = open('data.txt', 'wb')
+    pickle.dump(data, file)
+    file.close()
