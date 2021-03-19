9169fe13e0bc515a66fe7fcd508b2b57cd56a1ba
Hugo <hugo.huet.14@gmail.com>
2021-03-19 19:34:56 +0100

    user connection

--- devices/sensors/service.py
+++ devices/sensors/service.py
@@ -2,17 +2,20 @@ import requests
 import pickle
 
 def callApi(data):
-    url = "https://lilseed-back.serveurspaul.duckdns.org/users/login"
-    data = {"username": "lilseed", "password": "password"}
-
-    # url = "https://lilseed-back.serveurspaul.duckdns.org/"
-    # lilseed
-    # password
-    # print("Call to API :", url, "with data :", data, "\n")
+    connUrl = "https://lilseed-back.serveurspaul.duckdns.org/users/login"
+    connData = {"username": "lilseed", "password": "password"}
     
     try:
-        res = requests.post(url, json = data)
-        print(res.json()["token"])
+        connReq = requests.post(connUrl, json = connData)
+        if len(connReq.json()["token"].strip()) > 0:
+            addPotUrl = "https://lilseed-back.serveurspaul.duckdns.org//users/" + connReq.json()["token"] + "pots"
+            addPotData = {
+                "name": "Pot de la kouisine",
+                "macAddress": "00:00:00:00"
+            }
+            print(addPotUrl)
+            addPotReq = requests.post(addPotUrl, json = addPotData)
+            print(addPotReq.json())
     except:
         pass
 
@@ -27,3 +30,5 @@ def storeData(data):
     file = open('data.txt', 'wb')
     pickle.dump(data, file)
     file.close()
+
+callApi({})
