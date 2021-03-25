23c509bd415f27f1a59f12a440716e30e1f24e7b
Matteo Lecuit <matteo@lecuit.fr>
2021-03-25 14:16:10 +0100

    working wifi server with flask

--- hardware/wifi/webserver.py
+++ hardware/wifi/webserver.py
@@ -4,15 +4,15 @@ from wifi import Cell, Scheme
 iwifi = 'wlan0'
 iname = 'home'
 app = Flask(__name__)
+cells = Cell.all(iwifi)
+cellsList = []
+for i, cell in enumerate(cells):
+    cellsList.append(cell)
+    print(i, ": ", cell)
+cells = Cell.all(iwifi)
 
 @app.route('/')
 def form():
-    cells = Cell.all(iwifi)
-    cellsList = []
-    for i, cell in enumerate(cells):
-        cellsList.append(cell)
-        print(i, ": ", cell)
-    cells = Cell.all(iwifi)
     return render_template('form.html', option_list=cellsList)
  
 @app.route('/data', methods = ['POST', 'GET'])
@@ -21,11 +21,12 @@ def data():
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
         form_data = request.form.to_dict()
-        ssid = form_data['Wifi']
+        index = int(form_data['Wifi'])
+        print(index)
         password = form_data['Password']
         scheme = None
         try:
-            scheme = Scheme.for_cell(iwifi, 'home', Cell.from_string(ssid), password)
+            scheme = Scheme.for_cell(iwifi, 'home', cellsList[index], password)
             scheme.save()
         except AssertionError:
             print('Scheme already exists')
